00d445b801970bba9d75cc6647ccbe2e109a005c
vinita04 <vinita.a1996@gmail.com>
2024-03-01 00:13:05 +0530

    testing cloud build

--- src/main/java/com/nagp/ecommerce/dto/SearchDTO.java
+++ src/main/java/com/nagp/ecommerce/dto/SearchDTO.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
